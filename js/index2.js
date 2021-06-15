488736719f1a312f6e225ddcafbb101718f76b2e
Yana Sokurenko <yanamoss@gmail.com>
2021-06-15 22:59:21 +0300

    Второй вариант слайда

--- js/index2.js
+++ js/index2.js
@@ -9,29 +9,29 @@ const lightboxOverlay = document.querySelector(".lightbox__overlay");
 let setItemGallery = " ";
 
 // Добавляю элементы в html.
+let idx = 0;
 galleryItems.forEach(item => {
-   setItemGallery += (`<li class = "gallery__item"><a class = "gallery__link" href = "${item.original}"><img class = "gallery__image" src = "${item.preview}" alt = "${item.description}" data-source = "${item.original}"></a></li>`);
+   setItemGallery += (`<li class = "gallery__item"><a class = "gallery__link" href = "${item.original}"><img class = "gallery__image" src = "${item.preview}" alt = "${item.description}" data-source = "${item.original}" data-idx="${idx}"></a></li>`);
+   idx += 1;
 });
 gallery.insertAdjacentHTML('afterbegin', setItemGallery);
 
 // Добавляю слушателей
 gallery.addEventListener('click', openImage);
-// lightboxButton.addEventListener('click', closeImage);
-// lightboxOverlay.addEventListener('click', closeImage);
-// window.addEventListener('keyup', keyboard);
-// window.addEventListener('keyup', slider);
+
 
 // Функция открытия картинки в оригинальном размере
-function openImage(event) {
+function openImage(event) { 
    event.preventDefault();
    ligthbox.classList.add('is-open');
    lightboxImage.setAttribute('src', event.target.getAttribute('data-source'));
    lightboxImage.setAttribute('alt', event.target.getAttribute('alt'));
+   lightboxImage.setAttribute('data-idx', event.target.getAttribute('data-idx'));
    lightboxButton.addEventListener('click', closeImage);
    lightboxOverlay.addEventListener('click', closeImage);
    window.addEventListener('keyup', keyboard);
    window.addEventListener('keyup', slider);
-}; 
+};
 
 // Функция закрытия картинки
 function closeImage(event) {
@@ -41,9 +41,7 @@ function closeImage(event) {
    lightboxButton.removeEventListener('click', closeImage);
    lightboxOverlay.removeEventListener('click', closeImage);
    window.removeEventListener('keyup', keyboard);
-   window.removeEventListener('keyup', slider);
-   
-      
+   window.removeEventListener('keyup', slider);     
 };
 
 // Закрытие по "Esc"
@@ -57,31 +55,24 @@ function keyboard(event) {
 
 function slider(event) {
    if (event.key === "ArrowLeft") {
-      swipeLeft(galleryItems);
+      swipe(galleryItems, -1);
    };
     if (event.key === "ArrowRight") {
-      swipeRight(galleryItems);
+       swipe(galleryItems, 1);
    };
 };
 
-function swipeLeft(galleryItems) {
-         for (let index = 0; index < galleryItems.length; index++) {
-            const elementGallery = galleryItems[index];
-            if (elementGallery.original === lightboxImage.getAttribute("src")&& index !==0) {
-             lightboxImage.setAttribute('src', galleryItems[(index - 1)].original);
-             lightboxImage.setAttribute('alt', galleryItems[(index - 1  )].description);  
-            };
+function swipe(galleryItems, delta) {
+   let index = lightboxImage.getAttribute("data-idx");
+   let indexSlider = Number(index) + delta;
+   if (indexSlider < 0) {
+      indexSlider = galleryItems.length - 1;
    };
-         
+   if (indexSlider === galleryItems.length) {
+      indexSlider = 0;
+   }
+   lightboxImage.setAttribute('src', galleryItems[indexSlider].original);
+   lightboxImage.setAttribute('alt', galleryItems[indexSlider].description);
+   lightboxImage.setAttribute('data-idx', indexSlider);      
 };
 
-function swipeRight(galleryItems) {
-         for (let index = 0; index < galleryItems.length; index++) {
-            const elementGallery = galleryItems[index];
-            if (elementGallery.original === lightboxImage.getAttribute("src")&& index !== (galleryItems.length -1)) {
-            lightboxImage.setAttribute('src', galleryItems[(index + 1)].original);
-            lightboxImage.setAttribute('alt', galleryItems[(index + 1)].description);
-               break;
-            };
-   };        
-};
